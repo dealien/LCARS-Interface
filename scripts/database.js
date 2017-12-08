bcc418c73cf0963b25958499d5a28aa0bc81c867
dealien <dragonofawsome@gmail.com>
2017-12-07 21:42:35 -0500

    Added basic project files

---
+++ scripts/database.js
@@ -0,0 +1,25 @@
+const dataFolder = './data/';
+const fs = require('fs');
+
+function walk(currentDirPath, callback) {
+  var fs = require('fs'),
+    path = require('path');
+  fs.readdir(currentDirPath, function(err, files) {
+    if (err) {
+      throw new Error(err);
+    }
+    files.forEach(function(name) {
+      var filePath = path.join(currentDirPath, name);
+      var stat = fs.statSync(filePath);
+      if (stat.isFile()) {
+        callback(filePath, stat);
+      } else if (stat.isDirectory()) {
+        walk(filePath, callback);
+      }
+    });
+  });
+}
+
+walk('./data', function(filePath, stat) {
+  console.log(filePath);
+});
