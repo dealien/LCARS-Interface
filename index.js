caa64a3d07fea6553c7bb1ee54d895202c7754ac
Unknown <dragonofawsome@gmail.com>
2017-12-11 20:48:00 -0500

    Added web server test to Travis CI test/build process

--- index.js
+++ index.js
@@ -32,3 +32,36 @@ app.engine('.hbs', exphbs({
 }))
 app.set('view engine', '.hbs')
 app.set('views', path.join(__dirname, 'views'))
+
+// ########## DATABASE FUNCTIONS ##########
+
+function listFiles() {
+    var fs = require('fs'),
+        path = require('path');
+    fs.readdir('./data/', function(err, files) {
+        if (err) {
+            throw new Error(err);
+        }
+        files.forEach(function(name) {
+            var filePath = path.join('./data/', name);
+            var stat = fs.statSync(filePath);
+            if (stat.isFile()) {
+                callback(filePath, stat);
+            } else if (stat.isDirectory()) {
+                walk(filePath, callback);
+            }
+        });
+    });
+}
+
+function scan() {
+    var files = [];
+    walk('./data/', function(filePath, stat) {
+        files.push(filePath.toString());
+    });
+}
+// },
+// load: function() {
+
+// }
+}
