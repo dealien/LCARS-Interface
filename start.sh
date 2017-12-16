a8b22f54292fff12f2b22f1cec0e7295bf2db280
dealien <dragonofawsome@gmail.com>
2017-12-16 16:35:46 -0500

    Fixed issue with forever log location
    Added forever.log and error.log, and ensured that server.log, error.log, and forever.log have matching version numbers.

--- start.sh
+++ start.sh
@@ -1,12 +1,20 @@
+#!/bin/bash
+
 if [ ! -d logs/ ]; then
     mkdir logs/
 fi
-if [ -f logs/server.log ]; then
+
+# Ensure that "server.log" and "forever.log" have matching version numbers
+if [ -f logs/server.log ] || [ -f logs/forever.log ]; then
     let "n=2"
     while [ -f logs/server${n}.log ]; do
-        let "n=n++"
+        let "sn=sn++"
+    done
+    while [ -f logs/forever${n}.log ]; do
+        let "sn=sn++"
     done
 fi
-echo Logging to logs/server${n}.log
 
-forever start -o server${n}.log server.js
+echo Logging to:\nlogs/server${n}.log\nlogs/forever${n}.log
+
+forever start -l logs/forever${n}.log -o logs/server${n}.log server.js
