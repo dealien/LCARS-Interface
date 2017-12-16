6d35d19c5f60d0bb2cfcd273b5f9313f0a3918ef
dealien <dragonofawsome@gmail.com>
2017-12-16 17:35:22 -0500

    Rebuilt manager.sh

--- manager.sh
+++ manager.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-help="Usage: `basename $0` <command> -- manage the LCARS Database server
+htext="Usage: `basename $0` <command> -- manage the LCARS Database server
 
     `basename $0` start         starts the server
     `basename $0` stop          stops the server
@@ -8,44 +8,48 @@ help="Usage: `basename $0` <command> -- manage the LCARS Database server
         
     `basename $0` (-h|--help)   displays this help text"
 
-if [ $1 -eq "start"]; then
-    if [ ! -d logs/ ]; then
+echo Beginning of manager script
+if [[ "$1" = "start" ]]; then
+    if [[ ! -d logs/ ]]; then
         mkdir logs/
     fi
 
-    # Ensure that "server.log", "error.log", and "forever.log" have matching version numbers
-    if [ -f logs/server.log ] || [ -f logs/forever.log ]; then
-        let "n=2"
-        while [ -f logs/server${n}.log ]; do
-            let "sn=sn++"
-        done
-        while [ -f logs/error${n}.log ]; do
-            let "sn=sn++"
-        done
-        while [ -f logs/forever${n}.log ]; do
-            let "sn=sn++"
-        done
+    # Set up logging and preserve old log files
+    if [ -f $(pwd)/logs/server.log ]; then
+        if [[ ! -d logs/old/ ]]; then
+            mkdir logs/old/
+        fi
+        n=1
+        if [ -f $(pwd)/logs/old/server${n}.log ]; then
+            while [ -f $(pwd)/logs/old/server${n}.log ]; do
+                let sn=$((n++))
+            done
+
+        fi
+        mv $(pwd)/logs/server.log $(pwd)/logs/old/server${n}.log
     fi
 
-    echo Logging to:\nlogs/server${n}.log\nlogs/error${n}.log\nlogs/forever${n}.log
+    echo Logging to $(pwd)/logs/server.log
+    echo "Starting server..."
 
-    forever start -l 'logs/forever${n}.log -e logs/error${n}.log -o logs/server${n}.log server.js'
+    # forever start -l $(pwd)/logs/server.log -e $(pwd)/logs/error.log -o $(pwd)/logs/server.log server.js
+    forever start -l $(pwd)/logs/server.log server.js
 
-elif [ $1 -eq "stop" ]; then
+elif [[ "$1" = "stop" ]]; then
     echo Stopping server...
 
     forever stop server.js
 
     echo Server stopped
 
-elif [ $1 -eq "info" ]; then
+elif [[ "$1" = "info" ]]; then
     forever list
 
-elif [ $1 -eq "-h" ] || [ $1 -eq "--help"]; then
-    echo "$help"
+elif [[ "$1" = "-h" ]] || [[ "$1" = "--help" ]]; then
+    echo "$htext"
     exit 0
 
 else
-    echo "$help"
+    echo "$htext"
     exit 0
 fi
